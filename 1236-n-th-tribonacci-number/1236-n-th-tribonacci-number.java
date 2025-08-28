4988ad05e0263ee56bab59419c73e7d971a475e4
Navanitha T <149072224+navanitha23@users.noreply.github.com>
2025-08-28 21:10:10 +0530

    Time: 0 ms (100%), Space: 40.2 MB (92.28%) - LeetHub

---
+++ 1236-n-th-tribonacci-number/1236-n-th-tribonacci-number.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int tribonacci(int n) {
+        if(n<3){
+            return n==0?0:1;
+        }
+        int a=0,b=1,c=1,res=0;
+        for(int i=3;i<=n;i++){
+            res=a+b+c;
+            a=b;
+            b=c;
+            c=res;
+        }
+        return res;
+    }
+}
