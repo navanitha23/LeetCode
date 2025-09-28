cb6032fa8adc5f294e950ba56ed7c4488e793684
Navanitha T <149072224+navanitha23@users.noreply.github.com>
2025-09-28 18:33:52 +0530

    Time: 33 ms (30.7%), Space: 55.1 MB (82.65%) - LeetHub

---
+++ 0937-online-stock-span/0937-online-stock-span.java
@@ -0,0 +1,26 @@
+class StockSpanner {
+        Stack<Pair> st;
+        int index;
+
+    public StockSpanner() {
+        st = new Stack<>();
+        index = -1;
+    }
+    
+    public int next(int price) {
+        index = index +1;
+        while(!st.empty() && st.peek().price <= price){
+            st.pop();
+        }
+        int ans = index - (st.empty() ? -1 : st.peek().ind);
+        st.push(new Pair(price, index));
+        return ans;
+    }
+}
+class Pair{
+    int price, ind;
+    Pair(int price, int ind){
+        this.price = price;
+        this.ind = ind;
+    }
+}
